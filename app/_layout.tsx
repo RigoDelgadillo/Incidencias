575348d64dc522ad300f16a0ccc23c9278737e9a
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2025-10-29 15:41:00 -0600

    Se instaló NativeWind para los estilos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,13 @@
 import { Text, View } from "react-native";
-import "react-native-reanimated";
 
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
+import "../global.css";
 
 export default function RootLayout() {
   return (
-    <View>
-      <Text>Hola mundo</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Hola mundo desde RootLayout con NativeWind!
+      </Text>
     </View>
   );
 }
